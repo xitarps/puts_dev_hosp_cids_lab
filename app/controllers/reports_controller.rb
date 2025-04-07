8ae6b9e1317f4c24ab6a4a1f8058ab17f1c2868e
XitaRps <xitarps@gmail.com>
2025-04-07 01:02:54 -0300

    Refac: Changes to ORM/Database data handle

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,15 +1,29 @@
 class ReportsController < ApplicationController
   def by_hosp
-    @hosp_id = params[:hosp_id]
-    @hosp_user_cids = HospitalUserCid.all
+    @hosp_user_cids = HospitalUserCid.select(cid: [ :number, :description ],
+                                             hospital: [ :name ])
+                                     .select("COUNT(user_cids.id) AS quantity")
+                                     .joins(:hospital, user_cid: :cid)
+                                     .where(hospital_id: params[:hosp_id].to_i)
+                                     .group(user_cids: :cid_id,
+                                            cid: [ :number, :description ],
+                                            hospital: [ :name ])
+                                     .order(cid_id: :asc)
   end
 
   def by_user
-    @user_id = params[:user_id]
-    @user_cids = UserCid.all
+    @user_cids = UserCid.select(cids: [ :number, :description ],
+                                hospitals: [ :name ])
+                        .joins(:cid, hospital_user_cids: :hospital)
+                        .where(user_id: params[:user_id])
+                        .order(user_id: :desc)
   end
 
   def overall
-    @user_cids = UserCid.all
+    @user_cids = UserCid.select("COUNT(cids.id) AS total",
+                                cids: [ :number, :description ])
+                        .joins(:cid)
+                        .group(cids: :id)
+                        .order(cids: { id: :asc })
   end
 end
